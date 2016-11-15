5f2eb6fb95e95cb1724e402714ef0500224f0025
ejatkin <ejatkin@gmail.com>
2016-11-15 15:26:18 +0000

    Completed ex 6 - added max balance feature

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,15 @@ class Oystercard
 
 attr_reader :balance
 
+DEFAULT_OYSTER_MAX_BALANCE = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(value)
-    @balance =+ value
+    raise "Error: top up failed, maximum value cannot exceed #{DEFAULT_OYSTER_MAX_BALANCE}" if @balance + value > DEFAULT_OYSTER_MAX_BALANCE
+    @balance += value
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,4 +14,8 @@ describe Oystercard do
     expect(subject.balance).to eq 5
   end
 
+  it "stops the card being topped up over a maximum value of £90" do
+    expect { (subject.top_up(95)) }.to raise_error "Error: top up failed, maximum value cannot exceed #{subject.class::DEFAULT_OYSTER_MAX_BALANCE}"
+  end
+
 end
