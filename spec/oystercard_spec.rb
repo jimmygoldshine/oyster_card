4ccd30773f8a5b9d5531ef1aabe639436250c0af
ejatkin <ejatkin@gmail.com>
2016-11-15 12:28:30 +0000

    Added default balance to zero - initialize method and attr_reader

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,15 @@
 require './lib/oystercard'
 
-describe do
+describe Oystercard do
+  it "checks a new card has a default balance of zero" do
+    expect(subject.balance).to eq 0
+  end
+
+
+
+
+  # In order to use public transport
+  # As a customer
+  # I want money on my card
 
 end
