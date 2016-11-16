fe34bfa2daed94df099a128111057db3d0cde304
James <jamesdix@cwgsy.net>
2016-11-16 11:41:57 +0000

    step 9 complete: minimum fare feature added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,6 +3,7 @@ class Oystercard
 attr_reader :balance, :in_use
 
 DEFAULT_OYSTER_MAX_BALANCE = 90
+MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
@@ -10,15 +11,25 @@ DEFAULT_OYSTER_MAX_BALANCE = 90
   end
 
   def top_up(value)
-    raise "Error: top up failed, maximum value cannot exceed #{DEFAULT_OYSTER_MAX_BALANCE}" if @balance + value > DEFAULT_OYSTER_MAX_BALANCE
+    raise "Error: top up failed, maximum value cannot exceed #{DEFAULT_OYSTER_MAX_BALANCE}" if balance_will_be_over_maximum(value)
     @balance += value
   end
 
-  def deduct(value)
-    @balance -= value
+  def balance_will_be_over_maximum(value)
+    @balance + value > DEFAULT_OYSTER_MAX_BALANCE
+  end
+
+  def balance_is_under_minimum_fare?
+    @balance < MINIMUM_FARE
+  end
+
+
+  def deduct(fare)
+    @balance -= fare
   end
 
   def touch_in
+    raise "Error: Not enough money on card for journey" if balance_is_under_minimum_fare?
     @in_use = true
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,52 +1,69 @@
 require './lib/oystercard'
 
 describe Oystercard do
+
+  let(:card) {described_class.new}
+
   it "checks a new card has a default balance of zero" do
-    expect(subject.balance).to eq 0
+    expect(card.balance).to eq 0
   end
 
   it "checks that top_up method can be passed one argument" do
-    expect(subject).to respond_to(:top_up).with(1).argument
+    expect(card).to respond_to(:top_up).with(1).argument
   end
 
   it "allows the balance of the card to increase by the top up value" do
-    subject.top_up(5)
-    expect(subject.balance).to eq 5
+    card.top_up(5)
+    expect(card.balance).to eq 5
   end
 
   it "stops the card being topped up over a maximum value of £90" do
-    expect { (subject.top_up(95)) }.to raise_error "Error: top up failed, maximum value cannot exceed #{subject.class::DEFAULT_OYSTER_MAX_BALANCE}"
-  end
-
-  it "checks that deduct method can be passed one argument" do
-    expect(subject).to respond_to(:deduct).with(1).argument
-  end
-
-  it "checks that it will deduct the fare from the oystercard balance" do
-    expect(subject.deduct(10)).to eq -10
-  end
-
-  it "confirms that the card has been touched in" do
-    expect(subject.touch_in).to eq true
-  end
-
-  it "confirms that the card has been touched out" do
-    expect(subject.touch_out).to eq true
+    expect { (card.top_up(95)) }.to raise_error "Error: top up failed, maximum value cannot exceed #{card.class::DEFAULT_OYSTER_MAX_BALANCE}"
   end
 
   # it "checks whether the card is part way through a journey" do
-  #   expect(subject.in_journey?).to eq true
+  #   expect(card.in_journey?).to eq true
   # end
 
-  it "checks if the card is in use" do
-    subject.touch_in
-    expect(subject.in_use).to eq true
+  context "Not enough money on Oystercard" do
+
+    it "raises an error when touch in without enough money" do
+      expect{card.touch_in}.to raise_error("Error: Not enough money on card for journey")
+    end
   end
 
-  it "checks if the card has stopped being in use" do
-    subject.touch_in
-    subject.touch_out
-    expect(subject.in_journey?).to eq false
+  context "Enough money on Oystercard for journey" do
+
+    before do
+      card.top_up(1)
+    end
+
+    it "checks if the card is in use" do
+      card.touch_in
+      expect(card.in_use).to eq true
+    end
+
+    it "checks if the card has stopped being in use" do
+      card.touch_in
+      card.touch_out
+      expect(card.in_journey?).to eq false
+    end
+
+    it "confirms that the card has been touched in" do
+      expect(card.touch_in).to eq true
+    end
+
+    it "checks that deduct method can be passed one argument" do
+      expect(card).to respond_to(:deduct).with(1).argument
+    end
+
+    it "checks that it will deduct the fare from the oystercard balance" do
+      expect{card.deduct(3)}.to change{ card.balance }.by -3
+    end
+
+    it "confirms that the card has been touched out" do
+      expect(card.touch_out).to eq true
+    end
   end
 
 end
